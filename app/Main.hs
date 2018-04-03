f121cddd9ab16841dc23ed945f563c42474a00dc
bouzuya <m@bouzuya.net>
2018-04-03 22:40:51 +0900

    getEnv

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main
   ( main
   )
 where
 
-import           Lib (someFunc)
+import           Lib                (someFunc)
+import           System.Environment (getEnv)
 
 main :: IO ()
-main = someFunc
-
+main = do
+  accessKeyToken <- getEnv "HS3_ACCESS_KEY_ID"
+  bucketName <- getEnv  "HS3_BUCKET_NAME"
+  secretAccessKey <- getEnv  "HS3_SECRET_ACCESS_KEY"
+  putStrLn $ "AccessKeyId: " ++ accessKeyToken
+  putStrLn $ "BucketName: " ++ bucketName
+  putStrLn $ "SecretAccessKey: " ++ secretAccessKey
