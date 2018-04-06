39581048a920f90d9899ceb2e203544024fc41ec
bouzuya <m@bouzuya.net>
2018-04-06 21:53:12 +0900

    print body

--- app/Main.hs
+++ app/Main.hs
@@ -4,19 +4,23 @@ module Main
   ( main
   )
 where
-import           Control.Lens       ((^.))
-import           Data.Either        (Either (Left, Right))
-import           Data.Text          (Text, pack, unpack)
-import           Network.AWS        (AccessKey, Credentials (FromKeys),
-                                     Region (Tokyo), SecretKey, newEnv, runAWS,
-                                     runResourceT, send, within)
-import           Network.AWS.Data   (fromText)
-import           Network.AWS.S3     (BucketName (BucketName),
-                                     ObjectKey (ObjectKey), getObject,
-                                     gorsContentType, gorsResponseStatus)
-import           Prelude            (IO, String, error, print, pure, putStrLn,
-                                     ($), (++), (<$>))
-import           System.Environment (getEnv)
+
+import           Control.Lens               ((^.))
+import qualified Data.ByteString.Lazy.Char8 as B
+import           Data.Conduit.Binary        (sinkLbs)
+import           Data.Either                (Either (Left, Right))
+import           Data.Text                  (Text, pack, unpack)
+import           Network.AWS                (AccessKey, Credentials (FromKeys),
+                                             Region (Tokyo), SecretKey, newEnv,
+                                             runAWS, runResourceT, send,
+                                             sinkBody, within)
+import           Network.AWS.Data           (fromText)
+import           Network.AWS.S3             (BucketName (BucketName),
+                                             ObjectKey (ObjectKey), getObject,
+                                             gorsBody)
+import           Prelude                    (IO, String, error, pure, putStrLn,
+                                             ($), (++), (.), (<$>))
+import           System.Environment         (getEnv)
 
 fromKeys' :: Text -> Text -> Either String Credentials
 fromKeys' accessKeyIdText secretAccessKeyText = do
@@ -29,15 +33,16 @@ main = do
   accessKeyIdText <- pack <$> getEnv "HS3_ACCESS_KEY_ID"
   bucketNameText <- pack <$> getEnv  "HS3_BUCKET_NAME"
   secretAccessKeyText <- pack <$> getEnv  "HS3_SECRET_ACCESS_KEY"
-  putStrLn $ "AccessKeyId: " ++ (unpack accessKeyIdText)
-  putStrLn $ "BucketName: " ++ (unpack bucketNameText)
-  putStrLn $ "SecretAccessKey: " ++ (unpack secretAccessKeyText)
+  putStrLn $ "AccessKeyId: " ++ unpack accessKeyIdText
+  putStrLn $ "BucketName: " ++ unpack bucketNameText
+  putStrLn $ "SecretAccessKey: " ++ unpack secretAccessKeyText
   let objectKey = ObjectKey "hello.txt"
   let bucketName = BucketName bucketNameText
   env <- case fromKeys' accessKeyIdText secretAccessKeyText of
     (Left _)            -> error "invalid credentials"
     (Right credentials) -> newEnv credentials
   let request = getObject bucketName objectKey
-  response <- runResourceT $ runAWS env $ within Tokyo $ send request
-  print $ response ^. gorsResponseStatus
-  print $ response ^. gorsContentType
+  lbs <- runResourceT . runAWS env $ within Tokyo $ do
+    response <- send request
+    sinkBody (response ^. gorsBody) sinkLbs
+  putStrLn $ B.unpack lbs
